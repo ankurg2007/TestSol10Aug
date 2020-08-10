279e700a6bc1dcb94ef353f1b3b359a29b5f9948
ankurg2007 <50264477+ankurg2007@users.noreply.github.com>
2020-08-10 13:58:48 +0530

    Unit Testing- TDD Approach- commit no 3

---
+++ UnitTestProject1/NotificationRepoTests.cs
@@ -0,0 +1,28 @@
+﻿using Business_Rules_Engine;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject1.UnitTests
+{
+
+    [TestClass()]
+    class NotificationRepoTests
+    {
+
+
+        [TestMethod()]
+        public void ActivationUpgradeNotifcationTest()
+        {
+            NotificationRepo notificationRepo = new NotificationRepo();
+
+            var result = notificationRepo.ActivationUpgradeNotifcation();
+            Assert.AreEqual(result, true);
+        }
+
+      
+    }
+}
